45c681eadcc060b204f18c259b07a7ce5aa66af7
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-10 09:52:31 +0500

    heading text component

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Heading } from '../components';
+
+const Index = () => {
+  return (
+    <div>
+      <Heading tag="h1">Heading</Heading>
+      <Heading tag="h2">Heading</Heading>
+      <Heading tag="h3">Heading</Heading>
+    </div>
+  );
+};
+
+export default Index;
