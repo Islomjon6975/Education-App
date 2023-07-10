98cfc023000cf9e15ba28f817414b1591b89b6cc
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-10 10:44:36 +0500

    text component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading } from '../components';
+import { Heading, Text } from '../components';
 
 const Index = () => {
   return (
@@ -7,6 +7,10 @@ const Index = () => {
       <Heading tag="h1">Heading</Heading>
       <Heading tag="h2">Heading</Heading>
       <Heading tag="h3">Heading</Heading>
+      <hr />
+      <Text size="s">Text</Text>
+      <Text size="m">Text</Text>
+      <Text size="l">Text</Text>
     </div>
   );
 };
