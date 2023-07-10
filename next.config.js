2e82c7047af59b87574e28baa2f00f1aa0be9127
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-10 09:31:08 +0500

    NextAPP, Config TS & Eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ['media.graphassets.com'],
+    dangerouslyAllowSVG: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+export default nextConfig;
