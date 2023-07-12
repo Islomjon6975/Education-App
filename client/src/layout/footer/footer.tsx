c131222a6643e83edd7895ceffc1c43569756a48
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-12 06:55:03 +0500

    Footer component

--- client/src/layout/footer/footer.tsx
+++ client/src/layout/footer/footer.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
 import { FooterProps } from './footer.props';
+import styles from './footer.module.css';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <footer {...props}>Footer</footer>;
+const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>
+        Education © 2023 - {+format(new Date(), 'yyyy') + 1}. All right reserved{' '}
+      </div>
+      <a href="#">Terms of use</a>
+      <a href="#">Privacy policy</a>
+    </footer>
+  );
 };
 
 export default Footer;
