93f2a6b9e9f333656711ec58eac5d748da54f29c
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-12 06:01:22 +0500

    shablon

--- client/src/layout/layout.tsx
+++ client/src/layout/layout.tsx
@@ -3,17 +3,16 @@ import { LayoutProps } from './layout.props';
 import Header from './header/header';
 import Sidebar from './sidebar/sidebar';
 import Footer from './footer/footer';
+import styles from './layout.module.css';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
@@ -21,8 +20,6 @@ export const withLayout = <T extends Record<string, unknown>>(
   Component: FunctionComponent<T>
 ) => {
   return function withLayoutComponent(props: T): JSX.Element {
-    console.log(props, 'props');
-
     return (
       <Layout>
         <Component {...props} />
