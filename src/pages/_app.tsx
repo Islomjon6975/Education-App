8f1f0b86036196c82e9367928f2dd40929696475
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-07-18 14:54:57 +0500

    NProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css';
+import * as React from 'react';
 import { AppProps } from 'next/app';
+import '../styles/globals.css';
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+NProgress.configure({ showSpinner: false });
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on('routeChangeStart', handleRouteStart);
+    Router.events.on('routeChangeComplete', handleRouteDone);
+    Router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      Router.events.off('routeChangeStart', handleRouteStart);
+      Router.events.off('routeChangeComplete', handleRouteDone);
+      Router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
